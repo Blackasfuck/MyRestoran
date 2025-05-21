b965a609cf017d41a92bf0a526ebba4760356398
Momonga <animenatsy290@gmail.com>
2025-05-21 14:37:56 +0500

    Merge remote-tracking branch 'origin/master'

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig(({ mode }) => ({
   base: '/', // <--- если сайт в корне, оставь '/'
   plugins: [
